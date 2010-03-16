6881d6cd9ca2ccf448aea8800aaf3877e2202adb
Wei Wang <weiwang.udel@gmail.com>
2010-03-16 21:37:55 +0000

    g_boundary to 1000000

--- Anjuta_SamHis/src/main.c
+++ Anjuta_SamHis/src/main.c
@@ -34,7 +34,7 @@ double est_total;
 double est_num;
 long int already_covered = 0;
 long int newly_covered = 0;
-long int g_boundary_num = 1000000000;
+long int g_boundary_num = 1000000;
 int g_dq_times;		/* dq iteration loop */
 long int g_dq_threshold;  /* dq threshold */
 int g_large_used =0; /* large directory encountered  */
@@ -55,7 +55,7 @@ struct dir_node
 
 void CleanExit(int sig);
 void get_all_subdirs
-(const char *path, struct dir_node *, int *, int *);
+(const char *path, struct dir_node *, long int *, long  int *);
 static char *dup_str(const char *s);
 double GetResult();
 int begin_sample_from(const char *root, struct dir_node *, double);
@@ -128,7 +128,7 @@ int main(int argc, char **argv)
 	//srand(1268762958);
 	//srand can also be used like this
 	//srand(2); 
-	printf("seed%d\n", seed);
+
 	int * depth_array = malloc(sample_times * sizeof (int));
 	long int * time_array = malloc (sample_times * sizeof (long int ));
 	
@@ -200,7 +200,7 @@ int begin_sample_from(
 		//printf("1\n");
 
 		fast_subdirs(cur_parent, curPtr);
-
+		//get_all_subdirs(cur_parent, curPtr, &sub_dir_num, &sub_file_num);
 		//printf("2\n");
 
 		/* sdirStruct not null sounds not necessary to check! */
@@ -310,8 +310,8 @@ void anomaly_processing(struct dir_node *curPtr, double prob)
 void get_all_subdirs(   
     const char *path,               /* path name of the parent dir */
     struct dir_node *curPtr,  /* */
-	int *sub_dir_num,		        /* number of sub dirs */
-	int *sub_file_num)		        /* number of sub files */
+	long int *sub_dir_num,		        /* number of sub dirs */
+	long int *sub_file_num)		        /* number of sub files */
 {
     DIR *dir;
     struct dirent *pdirent;
